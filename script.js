3752f3de2b2716ea2d78d8fdc548737af7063df0
adwaithek <adwaithek01@gmail.com>
2022-12-09 00:04:52 +0530

    fin

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
 const form = document.querySelector("form")
 const search = document.querySelector("#search")
@@ -5,6 +6,9 @@ const weather = document.querySelector("#weather")
     // const API = `https://api.openweathermap.org/data/2.5/weather?
     // q=${city}&appid=${API_KEY}&units=metric`
     // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+
+
+    
 const getWeather = async(city) => {
     weather.innerHTML = `<h2> Loading... <h2>`
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
@@ -36,3 +40,4 @@ form.addEventListener(
         event.preventDefault();
     }
 )
+    
